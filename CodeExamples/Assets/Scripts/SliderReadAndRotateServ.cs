78d3b546caa41064ca8e5e068f838818e9222ea5
alexey ayusheev <ayusheev@lb24.ru>
2019-02-01 01:52:40 +0800

    актуализация кода

---
+++ CodeExamples/Assets/Scripts/SliderReadAndRotateServ.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[HelpURL("https://docs.unity3d.com/ScriptReference/HeaderAttribute.html")]
+public class SliderReadAndRotateServ : MonoBehaviour {
+
+    [HideInInspector]
+    public int HidedInt;
+
+    [SerializeField]
+	private int ShownInInspectorPrivateInt;
+
+    
+    [Header("Health Settings")]
+    public int health = 0;
+    public int maxHealth = 100;
+
+    public enum RotationAxisEnum { x, y, z };
+	public RotationAxisEnum RotationAxis;
+
+
+
+
+
+    public List<Item> items;
+
+	void Start () {
+		
+	}
+	 
+}
+
+[System.Serializable]
+public class Item
+{
+    public int Id;
+    public string Name;
+    public Sprite Image;
+}
